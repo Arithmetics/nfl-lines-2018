adf6b3d185c7829466ae0efac389ea06a4a6e65a
Arithmetics <brock.m.tillotson@gmail.com>
2020-07-29 20:18:57 -0700

    2020 upgrades

--- nfl.rb
+++ nfl.rb
@@ -1,5 +1,4 @@
-require 'csv'
-
+require "csv"
 
 ############## classes #############
 class Team
@@ -24,12 +23,11 @@ class Team
     if home
       wins_earned += win_share(diff, true)
     else
-      wins_earned =+ win_share(diff, false)
+      wins_earned = +win_share(diff, false)
     end
 
     @wins += wins_earned
   end
-
 end
 
 ########## functions ############
@@ -55,7 +53,7 @@ def win_share(diff, home)
     else
       0.1
     end
-  else 
+  else
     if diff > 5
       0.9
     elsif diff > 4
@@ -77,89 +75,87 @@ def win_share(diff, home)
     else
       0
     end
-  end 
+  end
 end
 
 def find_team_by_abv(arr, abv)
-  x = arr.select {|team| team.abrev == abv}
+  x = arr.select { |team| team.abrev == abv }
   x[0]
 end
 
 ########### variable declarations ###########
 
 teams = []
-weeks = CSV.read('nfl_sched.csv');
+weeks = CSV.read("nfl_sched_2020.csv")
 
-teams.push( Team.new( "Arizona Cardinals", "ARI", 5.5, 7 ) )
-teams.push( Team.new( "Atlanta Falcons", "ATL", 9, 2 ) )
-teams.push( Team.new( "Baltimore Ravens", "BAL", 8, 4 ) )
-teams.push( Team.new( "Buffalo Bills", "BUF", 6.5, 7 ) )
-teams.push( Team.new( "Carolina Panthers", "CAR", 9, 3 ) )
-teams.push( Team.new( "Chicago Bears", "CHI", 6.5, 5 ) )
-teams.push( Team.new( "Cinncinatti Bengals", "CIN", 6.5, 4 ) )
-teams.push( Team.new( "Cleveland Browns", "CLE", 6.5, 7 ) )
-teams.push( Team.new( "Dallas Cowboys", "DAL", 8.5, 4 ) )
-teams.push( Team.new( "Denver Broncos", "DEN", 7, 4 ) )
-teams.push( Team.new( "Detroit Lions", "DET", 7.5, 4 ) )
-teams.push( Team.new( "Green Bay Packers", "GB", 10, 2 ) )
-teams.push( Team.new( "Houston Texans", "HOU", 6.5, 5 ) )
-teams.push( Team.new( "Indianapolis Colts", "IND", 6.5, 4 ) )
-teams.push( Team.new( "Jacksonville Jaguars", "JAC", 9, 3 ) )
-teams.push( Team.new( "Kansas City Chiefs", "KC", 8.5, 4 ) )
-teams.push( Team.new( "Los Angeles Chargers", "LAC", 9.5, 2 ) )
-teams.push( Team.new( "Los Angeles Rams", "LAR", 10, 3 ) )
-teams.push( Team.new( "Miami Dolphins", "MIA", 6.5, 5 ) )
-teams.push( Team.new( "Minnesota Vikings", "MIN", 10, 3 ) )
-teams.push( Team.new( "New England Patriots", "NE", 11, 1 ) )
-teams.push( Team.new( "New Orleans Saints", "NO", 9.5, 3 ) )
-teams.push( Team.new( "New York Giants", "NYG", 7, 5 ) )
-teams.push( Team.new( "New York Jets", "NYJ", 6, 7 ) )
-teams.push( Team.new( "Oakland Raiders", "OAK", 8, 4 ) )
-teams.push( Team.new( "Philadelphia Eagles", "PHI", 10.5, 3 ) )
-teams.push( Team.new( "Pittsburgh Steelers", "PIT", 10.5, 3 ) )
-teams.push( Team.new( "Seattle Seahawks", "SEA", 8, 4 ) )
-teams.push( Team.new( "San Francisco 49ers", "SF", 8.5, 4 ) )
-teams.push( Team.new( "Tampa Bay Buccaneers", "TB", 6.5, 4 ) )
-teams.push( Team.new( "Tennessee Titans", "TEN", 8, 4 ) )
-teams.push( Team.new( "Washington Redskins", "WAS", 7, 5 ) )
+teams_to_create = [
+  { name: "Arizona Cardinals", abbr: "ARI", vegas_line: 5.5, tier: 7 },
+  { name: "Atlanta Falcons", abbr: "ATL", vegas_line: 9, tier: 3 },
+  { name: "Baltimore Ravens", abbr: "BAL", vegas_line: 8, tier: 4 },
+  { name: "Buffalo Bills", abbr: "BUF", vegas_line: 6.5, tier: 4 },
+  { name: "Carolina Panthers", abbr: "CAR", vegas_line: 9, tier: 4 },
+  { name: "Chicago Bears", abbr: "CHI", vegas_line: 6.5, tier: 4 },
+  { name: "Cinncinatti Bengals", abbr: "CIN", vegas_line: 6.5, tier: 6 },
+  { name: "Cleveland Browns", abbr: "CLE", vegas_line: 6.5, tier: 3 },
+  { name: "Dallas Cowboys", abbr: "DAL", vegas_line: 8.5, tier: 4 },
+  { name: "Denver Broncos", abbr: "DEN", vegas_line: 7, tier: 5 },
+  { name: "Detroit Lions", abbr: "DET", vegas_line: 7.5, tier: 5 },
+  { name: "Green Bay Packers", abbr: "GB", vegas_line: 10, tier: 2 },
+  { name: "Houston Texans", abbr: "HOU", vegas_line: 6.5, tier: 4 },
+  { name: "Indianapolis Colts", abbr: "IND", vegas_line: 6.5, tier: 4 },
+  { name: "Jacksonville Jaguars", abbr: "JAC", vegas_line: 9, tier: 4 },
+  { name: "Kansas City Chiefs", abbr: "KC", vegas_line: 8.5, tier: 1 },
+  { name: "Los Angeles Chargers", abbr: "LAC", vegas_line: 9.5, tier: 3 },
+  { name: "Los Angeles Rams", abbr: "LAR", vegas_line: 10, tier: 2 },
+  { name: "Miami Dolphins", abbr: "MIA", vegas_line: 6.5, tier: 7 },
+  { name: "Minnesota Vikings", abbr: "MIN", vegas_line: 10, tier: 3 },
+  { name: "New England Patriots", abbr: "NE", vegas_line: 11, tier: 2 },
+  { name: "New Orleans Saints", abbr: "NO", vegas_line: 9.5, tier: 1 },
+  { name: "New York Giants", abbr: "NYG", vegas_line: 7, tier: 7 },
+  { name: "New York Jets", abbr: "NYJ", vegas_line: 6, tier: 5 },
+  { name: "Oakland Raiders", abbr: "OAK", vegas_line: 8, tier: 6 },
+  { name: "Philadelphia Eagles", abbr: "PHI", vegas_line: 10.5, tier: 2 },
+  { name: "Pittsburgh Steelers", abbr: "PIT", vegas_line: 10.5, tier: 2 },
+  { name: "Seattle Seahawks", abbr: "SEA", vegas_line: 8, tier: 4 },
+  { name: "San Francisco 49ers", abbr: "SF", vegas_line: 8.5, tier: 5 },
+  { name: "Tampa Bay Buccaneers", abbr: "TB", vegas_line: 6.5, tier: 4 },
+  { name: "Tennessee Titans", abbr: "TEN", vegas_line: 8, tier: 5 },
+  { name: "Washington Redskins", abbr: "WAS", vegas_line: 7, tier: 7 },
+]
+
+teams_to_create.each do |team_stub|
+  teams.push(Team.new(team_stub[:name], team_stub[:abbr], team_stub[:vegas_line], team_stub[:tier]))
+end
 
 ######### scripts #########
 
 weeks.each do |week|
   focus_team = find_team_by_abv(teams, week[0])
   last_bye = false
-  week[1..-1].each_with_index do |game, i|puts i
+  week[1..-1].each_with_index do |game, i| puts i
     if game == "Bye"
-      last_bye = true
-      puts "bye week"
-    elsif game.split('@').length > 1
-      #team is on the road
-      last_bye = false
+    last_bye = true
+    puts "bye week"
+  elsif game.split("@").length > 1
+    #team is on the road
+    last_bye = false
 
-      oppo_team = find_team_by_abv(teams, game.split('@')[1])
+    oppo_team = find_team_by_abv(teams, game.split("@")[1])
 
-      focus_team.play_game(oppo_team, last_bye, false)
+    focus_team.play_game(oppo_team, last_bye, false)
 
-      puts "#{focus_team.name} @ #{oppo_team.name} and moves to #{focus_team.wins}"
-    else
-      #team is home
-      last_bye = false
-      oppo_team = find_team_by_abv(teams, game.split('@')[0])
+    puts "#{focus_team.name} @ #{oppo_team.name} and moves to #{focus_team.wins}"
+  else
+    #team is home
+    last_bye = false
+    oppo_team = find_team_by_abv(teams, game.split("@")[0])
 
-      focus_team.play_game(oppo_team, last_bye, true)
+    focus_team.play_game(oppo_team, last_bye, true)
 
-      puts "#{focus_team.name} home vs #{oppo_team.name} and moves to #{focus_team.wins}"
-    end
-  end   
+    puts "#{focus_team.name} home vs #{oppo_team.name} and moves to #{focus_team.wins}"
+  end   end
 end
 
 teams.each do |team|
   puts "#{team.name} --- #{team.wins.round(2)}"
 end
-
-
-
-
-
-
-
