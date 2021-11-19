7a5442abdc7b4afcc511c9b075d7f938116ffebe
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-11-19 09:25:15 -0800

    fix

--- nfl.rb
+++ nfl.rb
@@ -86,41 +86,41 @@ end
 ########### variable declarations ###########
 
 teams = []
-weeks = CSV.read("nfl_sched_2020.csv")
+weeks = CSV.read("nfl_sched_2021.csv")
 
 teams_to_create = [
-  { name: "Arizona Cardinals", abbr: "ARI", vegas_line: 5.5, tier: 7 },
-  { name: "Atlanta Falcons", abbr: "ATL", vegas_line: 9, tier: 3 },
-  { name: "Baltimore Ravens", abbr: "BAL", vegas_line: 8, tier: 4 },
-  { name: "Buffalo Bills", abbr: "BUF", vegas_line: 6.5, tier: 4 },
-  { name: "Carolina Panthers", abbr: "CAR", vegas_line: 9, tier: 4 },
-  { name: "Chicago Bears", abbr: "CHI", vegas_line: 6.5, tier: 4 },
-  { name: "Cinncinatti Bengals", abbr: "CIN", vegas_line: 6.5, tier: 6 },
-  { name: "Cleveland Browns", abbr: "CLE", vegas_line: 6.5, tier: 3 },
-  { name: "Dallas Cowboys", abbr: "DAL", vegas_line: 8.5, tier: 4 },
-  { name: "Denver Broncos", abbr: "DEN", vegas_line: 7, tier: 5 },
-  { name: "Detroit Lions", abbr: "DET", vegas_line: 7.5, tier: 5 },
-  { name: "Green Bay Packers", abbr: "GB", vegas_line: 10, tier: 2 },
-  { name: "Houston Texans", abbr: "HOU", vegas_line: 6.5, tier: 4 },
-  { name: "Indianapolis Colts", abbr: "IND", vegas_line: 6.5, tier: 4 },
-  { name: "Jacksonville Jaguars", abbr: "JAC", vegas_line: 9, tier: 4 },
-  { name: "Kansas City Chiefs", abbr: "KC", vegas_line: 8.5, tier: 1 },
+  { name: "Arizona Cardinals", abbr: "ARI", vegas_line: 8.5, tier: 4 },
+  { name: "Atlanta Falcons", abbr: "ATL", vegas_line: 7.5, tier: 5 },
+  { name: "Baltimore Ravens", abbr: "BAL", vegas_line: 11, tier: 2 },
+  { name: "Buffalo Bills", abbr: "BUF", vegas_line: 11, tier: 2 },
+  { name: "Carolina Panthers", abbr: "CAR", vegas_line: 7.5, tier: 5 },
+  { name: "Chicago Bears", abbr: "CHI", vegas_line: 7.5, tier: 4 },
+  { name: "Cinncinatti Bengals", abbr: "CIN", vegas_line: 6.5, tier: 5 },
+  { name: "Cleveland Browns", abbr: "CLE", vegas_line: 10.5, tier: 2 },
+  { name: "Dallas Cowboys", abbr: "DAL", vegas_line: 9, tier: 4 },
+  { name: "Denver Broncos", abbr: "DEN", vegas_line: 8.5, tier: 4 },
+  { name: "Detroit Lions", abbr: "DET", vegas_line: 4.5, tier: 6 },
+  { name: "Green Bay Packers", abbr: "GB", vegas_line: 10, tier: 3 },
+  { name: "Houston Texans", abbr: "HOU", vegas_line: 4, tier: 7 },
+  { name: "Indianapolis Colts", abbr: "IND", vegas_line: 9, tier: 4 },
+  { name: "Jacksonville Jaguars", abbr: "JAX", vegas_line: 6.5, tier: 5 },
+  { name: "Kansas City Chiefs", abbr: "KC", vegas_line: 12.5, tier: 1 },
+  { name: "Las Vegas Raiders", abbr: "LV", vegas_line: 7, tier: 6 },
   { name: "Los Angeles Chargers", abbr: "LAC", vegas_line: 9.5, tier: 3 },
-  { name: "Los Angeles Rams", abbr: "LAR", vegas_line: 10, tier: 2 },
-  { name: "Miami Dolphins", abbr: "MIA", vegas_line: 6.5, tier: 7 },
-  { name: "Minnesota Vikings", abbr: "MIN", vegas_line: 10, tier: 3 },
-  { name: "New England Patriots", abbr: "NE", vegas_line: 11, tier: 2 },
-  { name: "New Orleans Saints", abbr: "NO", vegas_line: 9.5, tier: 1 },
-  { name: "New York Giants", abbr: "NYG", vegas_line: 7, tier: 7 },
+  { name: "Los Angeles Rams", abbr: "LAR", vegas_line: 10.5, tier: 2 },
+  { name: "Miami Dolphins", abbr: "MIA", vegas_line: 9.5, tier: 4 },
+  { name: "Minnesota Vikings", abbr: "MIN", vegas_line: 9, tier: 4 },
+  { name: "New England Patriots", abbr: "NE", vegas_line: 9.5, tier: 4 },
+  { name: "New Orleans Saints", abbr: "NO", vegas_line: 9, tier: 3 },
+  { name: "New York Giants", abbr: "NYG", vegas_line: 7, tier: 6 },
   { name: "New York Jets", abbr: "NYJ", vegas_line: 6, tier: 5 },
-  { name: "Oakland Raiders", abbr: "OAK", vegas_line: 8, tier: 6 },
-  { name: "Philadelphia Eagles", abbr: "PHI", vegas_line: 10.5, tier: 2 },
-  { name: "Pittsburgh Steelers", abbr: "PIT", vegas_line: 10.5, tier: 2 },
-  { name: "Seattle Seahawks", abbr: "SEA", vegas_line: 8, tier: 4 },
-  { name: "San Francisco 49ers", abbr: "SF", vegas_line: 8.5, tier: 5 },
-  { name: "Tampa Bay Buccaneers", abbr: "TB", vegas_line: 6.5, tier: 4 },
-  { name: "Tennessee Titans", abbr: "TEN", vegas_line: 8, tier: 5 },
-  { name: "Washington Redskins", abbr: "WAS", vegas_line: 7, tier: 7 },
+  { name: "Philadelphia Eagles", abbr: "PHI", vegas_line: 6.5, tier: 5 },
+  { name: "Pittsburgh Steelers", abbr: "PIT", vegas_line: 8.5, tier: 4 },
+  { name: "Seattle Seahawks", abbr: "SEA", vegas_line: 10, tier: 4 },
+  { name: "San Francisco 49ers", abbr: "SF", vegas_line: 10.5, tier: 2 },
+  { name: "Tampa Bay Buccaneers", abbr: "TB", vegas_line: 12, tier: 1 },
+  { name: "Tennessee Titans", abbr: "TEN", vegas_line: 9, tier: 4 },
+  { name: "Washington Football Team", abbr: "WSH", vegas_line: 8.5, tier: 3 },
 ]
 
 teams_to_create.each do |team_stub|
@@ -132,8 +132,10 @@ end
 weeks.each do |week|
   focus_team = find_team_by_abv(teams, week[0])
   last_bye = false
-  week[1..-1].each_with_index do |game, i| puts i
-    if game == "Bye"
+  week[1..-1].each_with_index do |game, i| 
+    puts game
+    puts i
+    if game == "BYE"
     last_bye = true
     puts "bye week"
   elsif game.split("@").length > 1
@@ -157,5 +159,5 @@ weeks.each do |week|
 end
 
 teams.each do |team|
-  puts "#{team.name} --- #{team.wins.round(2)}"
+  puts "#{team.name}, #{team.wins.round(2)}"
 end
